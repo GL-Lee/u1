b9fe01d3d1d50fb3d979be1f864b467b37c3c86a
muhawo <muhawo@163.com>
2013-12-01 13:22:27 +0800

    编码问题解决

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var http = require('http');
 var path = require('path');
 var fs=require('fs');
 var app = express();
-var $ = require('jquery');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -25,41 +24,23 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+rulesDir = __dirname+ '/rules';
+dataDir = __dirname+ '/data';
+app.set('dataDir', path.join(__dirname, 'data'));
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-var data={};
-data.video = JSON.parse(fs.readFileSync('video.json', 'utf8'));
 
 app.get('/', routes.index);
 app.get('/users', user.list);
-app.get('/video', function(req, res){
-  res.render("video",data.video);
-});
+var video = require('./routes/video');
+app.get('/video', video.build);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
-http.get("http://www.youku.com", function(res) {
-  console.log("Got response: " + res.statusCode);
-  	var content = '';
-	res.on('data', function (chunk) {
-		content+=chunk.toString();
-	}).on('end', function(){
-		var con = $(content).find("a").attr("title").jion(":");
-		fs.writeFile('message.txt', con, function (err) {
-		  if (err) throw err;
-		  console.log('It\'s saved!');
-		});
-	 //    fs.open("test.txt","w",0644,function(e,fd){
-		//     fs.write(fd,content,0,content.length,0,function(e){
-		//         if(e) throw e;
-		//         fs.closeSync(fd);
-		//     })
-		// })
-	})
-}).on('error', function(e) {
-  console.log("Got error: " + e.message);
-});
+var data = require('./public/javascript/data');
+data.saveData();
+setTimeout(data.saveData,30*60*1000);
